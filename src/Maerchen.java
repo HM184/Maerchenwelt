b53fea5bd0188e9ed2683ac216a4ac44f07067a7
Melina Bregenzer <melina2399@gmx.net>
2021-01-26 19:39:12 +0100

    Aufgabe 1 done

--- src/Maerchen.java
+++ src/Maerchen.java
@@ -2,11 +2,6 @@ public class Maerchen {
 
     public static void main (String [] args){
         Maerchenwelt m = new Maerchenwelt(40,10,0,20);
-           for(int i = 0; i <101 ; i++){
                m.start();
-           }
-
-
-
     }
 }
